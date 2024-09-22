9086d11973e54b7a50309f756f647ecc7b9afb25
rotgier <rotgier@gmail.com>
2024-09-22 09:54:09 +0200

    Added _has_hourly_forecast_changed

--- custom_components/smart_rce/weather_listener.py
+++ custom_components/smart_rce/weather_listener.py
@@ -1,10 +1,19 @@
 """Weather Listener Coordinator."""
 
 from collections.abc import Callable
+from datetime import date, datetime
 import logging
 from typing import Final
 
-from homeassistant.components.weather import DOMAIN as WEATHER, WeatherEntity
+import aiofiles
+import orjson
+
+from homeassistant.components.weather import (
+    ATTR_FORECAST_TIME,
+    DOMAIN as WEATHER,
+    Forecast,
+    WeatherEntity,
+)
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     EVENT_HOMEASSISTANT_STARTED,
@@ -20,6 +29,7 @@ from homeassistant.core import (
 )
 from homeassistant.helpers.entity_component import EntityComponent
 from homeassistant.helpers.event import Event, async_track_state_change_event
+from homeassistant.util.dt import as_local, now as now_local
 from homeassistant.util.json import JsonValueType
 
 WEATHER_ENTITY: Final[str] = "weather.wetteronline"
@@ -49,6 +59,9 @@ class WeatherListenerCoordinator:
         self._shutdown_requested: bool = False
         self._listeners: dict[CALLBACK_TYPE, CALLBACK_TYPE] = {}
         self._unsubscribe_callback: CALLBACK_TYPE = None
+
+        self._last_hourly_forecast: list[Forecast] = None
+        self._last_hourly_forecast_day: date = None
 
         _LOGGER.debug("WeatherListenerCoordinator init")
         entry.async_on_unload(self._shutdown)
@@ -103,6 +116,7 @@ class WeatherListenerCoordinator:
             @callback
             def forecast_listener(forecast: list[JsonValueType] | None) -> None:
                 if not self._shutdown_requested:
+                    self._has_hourly_forecast_changed(forecast)
                     if self.forecast_hourly != forecast:
                         _LOGGER.debug("forecast_listener: forecast updated")
                         self.forecast_hourly = forecast
@@ -166,3 +180,68 @@ class WeatherListenerCoordinator:
         _LOGGER.debug("_shutdown")
         self._shutdown_requested = True
         self._unregister_weather_updates()
+
+    def _has_hourly_forecast_changed(self, forecast: list[Forecast]) -> bool:
+        now = now_local()
+
+        first_hour_raw = forecast[0][ATTR_FORECAST_TIME]
+        forecast_first_hour: datetime = as_local(datetime.fromisoformat(first_hour_raw))
+        forecast_day: date = forecast_first_hour.date()
+
+        if not self._last_hourly_forecast:
+            self._last_hourly_forecast = forecast
+            self._last_hourly_forecast_day = forecast_day
+            _LOGGER.debug("Save forecast because no last forecast")
+            self._save_forecast_to_file(now, forecast)
+            return True
+
+        last_hourly_forecast = self._last_hourly_forecast
+        len_difference = len(last_hourly_forecast) - len(forecast)
+
+        assert self._last_hourly_forecast_day
+        if forecast_day != self._last_hourly_forecast_day:
+            self._last_hourly_forecast = forecast
+            self._last_hourly_forecast_day = forecast_day
+            _LOGGER.debug("Save forecast because day differs")
+            self._save_forecast_to_file(now, forecast)
+            return True
+
+        if len_difference == 0:
+            if forecast != last_hourly_forecast:
+                _LOGGER.debug("Save forecast SAME size because it differs")
+                self._save_forecast_to_file(now, forecast)
+                return True
+            _LOGGER.debug("NO Save forecast SAME size because it is the same")
+            return False
+        if len_difference > 0:
+            assert (
+                last_hourly_forecast[len_difference][ATTR_FORECAST_TIME]
+                == forecast[0][ATTR_FORECAST_TIME]
+            )
+            assert (
+                last_hourly_forecast[len_difference + 1][ATTR_FORECAST_TIME]
+                == forecast[1][ATTR_FORECAST_TIME]
+            )
+            if forecast != last_hourly_forecast[len_difference:]:
+                _LOGGER.warning("Save forecast BIGGER size because it differs")
+                self._save_forecast_to_file(now, forecast)
+            else:
+                _LOGGER.warning("New smaller hourly_forecast is the same")
+            return True
+        _LOGGER.warning("Save forecast SMALLER size!!! SMALLER means sth is wrong")
+        _LOGGER.warning("SMALLER len_difference: %d", len_difference)
+        self._save_forecast_to_file(now, forecast)
+        return True
+
+    def _save_forecast_to_file(self, now: datetime, forecast: list[Forecast]) -> None:
+        _LOGGER.debug("_save_forecast_to_file")
+        self.hass.loop.create_task(self._async_save_forecast_to_file(now, forecast))
+
+    async def _async_save_forecast_to_file(
+        self, now: datetime, forecast: list[Forecast]
+    ) -> None:
+        _LOGGER.debug("_async_save_forecast_to_file")
+        raw_json = orjson.dumps(forecast, option=orjson.OPT_INDENT_2)
+        path = f"/config/smart_rce/hourly_forecast_{now.isoformat()}.json"
+        async with aiofiles.open(path, mode="w+", encoding="utf-8") as file:
+            await file.write(raw_json.decode("utf-8"))
